1a9377b0b419006ef916df5293f14d195152ca08
Camilo Campo <mcp.ccampo@gmail.com>
2021-01-09 09:46:11 -0500

    added and tested enpoints

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,17 +1,49 @@
 const data = [];
-let id = 0; 
+let id = 0;
 
 module.exports = {
   create: (req, res) => {
-
-  }, 
-  read: (req,res) => {
-
+    console.log(`~> Reached The Create End Point`);
+    const { time, text } = req.body;
+    const msg = { id, time, text };
+    data.push(msg);
+    id++;
+    console.log(` :: Added The Message to Array`);
+    res.status(200).send(data);
   },
-  update: (req,res) => {
-
+  read: (req, res) => {
+    console.log(`~> Reached The Read End Point`);
+    let msgData = [...data];
+    res.status(200).send(data);
   },
-  delete: (req,res) => {
-    
-  }
-}
+  update: (req, res) => {
+    console.log(`~> Reached The Update End Point`);
+
+    const { id } = req.params;
+    const { time, text } = req.body;
+
+    let index = data.findIndex((e) => e.id === parseInt(id));
+
+    let updateRecord = {
+      id: data[index].id,
+      time: time || data[index].time,
+      text: text || data[index].text,
+    };
+
+    data[index] = updateRecord;
+    res.status(200).send(data);
+  },
+  delete: (req, res) => {
+    console.log(`~> Reached The Delete End Point`);
+    const { id } = req.params;
+    let index = data.findIndex((e) => e.id === parseInt(id));
+    if (index === -1) {
+      res.status(404).send('Record not found');
+      console.log(` :: Record #${id} not found`);
+    } else {
+      data.splice(index, 1);
+      res.status(200).send(data);
+      console.log(` :: Deleted Record ${id}`);
+    }
+  },
+};
--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const PORT = 3050;
 const app = express();
 app.use(express.json());
 
+// Server Api Routes
+app.get('/api/message', msgCtr.read);
+app.post('/api/message', msgCtr.create);
+app.post('/api/message/:id', msgCtr.update);
+app.delete('/api/message/:id', msgCtr.delete);
+
 app.listen(PORT, () => {
   console.clear();
   console.log(`App started on port# ${PORT}`);
+  console.log('=========================');
 });
